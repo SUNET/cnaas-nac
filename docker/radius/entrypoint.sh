043775400e6aeae60c8a6f01cf13fb9042ca3954
Kristofer Hallin <kristofer@sunet.se>
2019-05-24 21:19:12 +0200

    * Fix entrypoint scripts

--- docker/radius/entrypoint.sh
+++ docker/radius/entrypoint.sh
@@ -1,14 +1,14 @@
 echo ">> Waiting for postgres to start"
-    WAIT=0
-    while ! nc -z nac_postgres 5432; do
-      sleep 1
-      WAIT=$(($WAIT + 1))
+WAIT=0
+while ! nc -z nac_postgres 5432; do
+    sleep 1
+    WAIT=$(($WAIT + 1))
       if [ "$WAIT" -gt 15 ]; then
-        echo "Error: Timeout wating for Postgres to start"
-        exit 1
+          echo "Error: Timeout wating for Postgres to start"
+          exit 1
       fi
-    done
-    
-psql -h docker_nac_postgres_1 -U cnaas nac -f /tmp/schema.sql > /tmp/psql.txt
+done
 
+psql -h nac_postgres -U cnaas nac -f /tmp/schema.sql
+ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled
 freeradius -X -f
