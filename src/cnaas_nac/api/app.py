79323f4d99e6576832a7a8e4fb7444cab184be87
Kristofer Hallin <kristofer@sunet.se>
2019-06-20 14:35:29 +0200

    Let urandom generate a secret key

--- src/cnaas_nac/api/app.py
+++ src/cnaas_nac/api/app.py
@@ -3,16 +3,21 @@ from flask_restful import Api
 from cnaas_nac.api.auth import AuthApi, AuthApiByName
 from cnaas_nac.api.webapi import WebApi
 
+import os
+
+
 API_VERSION = 'v1.0'
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config['SECRET_KEY'] = 'kakakekekeff'
+app.config['SECRET_KEY'] = os.urandom(128)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return WebApi.index()
-    
+
+
 api.add_resource(AuthApi, '/api/v1.0/auth')
 api.add_resource(AuthApiByName, '/api/v1.0/auth/<string:username>')
