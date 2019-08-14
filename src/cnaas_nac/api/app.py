f1e2e0b4ca576efd57b2612d66024f3a718bd4de
Kristofer Hallin <kristofer@sunet.se>
2019-08-14 14:52:51 +0200

    Remove web UI, moved to cnaas-front

--- src/cnaas_nac/api/app.py
+++ src/cnaas_nac/api/app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Api
 from cnaas_nac.api.auth import AuthApi, AuthApiByName
-from cnaas_nac.api.webapi import WebApi
 from cnaas_nac.version import __api_version__
 
 import os
@@ -14,11 +13,6 @@ app = Flask(__name__)
 api = Api(app)
 
 app.config['SECRET_KEY'] = os.urandom(128)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return WebApi.index()
 
 
 api.add_resource(AuthApi, f'/api/{ __api_version__ }/auth')
