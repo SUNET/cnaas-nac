5e3c76ba667168692fa60790b7ba5c62e00d87b9
Kristofer Hallin <kristofer@sunet.se>
2019-11-25 16:00:03 +0100

    Chech the OUI table if we should enable the device automatically or not.

--- src/cnaas_nac/db/user.py
+++ src/cnaas_nac/db/user.py
@@ -3,13 +3,54 @@ import ipaddress
 import datetime
 
 from typing import Optional
-from sqlalchemy import Column, Integer, Unicode, UniqueConstraint
+from sqlalchemy import Column, Integer, Unicode, UniqueConstraint, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Boolean
 from cnaas_nac.db.session import sqla_session
 
 
 Base = declarative_base()
+
+
+class PostAuth(Base):
+    __tablename__ = 'radpostauth'
+    __table_args__ = (
+        None,
+        UniqueConstraint('id'),
+    )
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    username = Column(Unicode(64), nullable=False)
+#    pass = Column(Unicode(64), nullable=False)
+    reply = Column(Unicode(64), nullable=False)
+#    CalledStationId = Column(Unicode(64), nullable=False)
+#    CallingStationId = Column(Unicode(64), nullable=False)
+    authdate = Column(DateTime, default=datetime.datetime.now)
+
+    def as_dict(self):
+        """Return JSON serializable dict."""
+        d = {}
+        for col in self.__table__.columns:
+            value = getattr(self, col.name)
+            if issubclass(value.__class__, enum.Enum):
+                value = value.value
+            elif issubclass(value.__class__, Base):
+                continue
+            elif issubclass(value.__class__, ipaddress.IPv4Address):
+                value = str(value)
+            elif issubclass(value.__class__, datetime.datetime):
+                value = str(value)
+            d[col.name] = value
+        return d
+
+    @classmethod
+    def get_last_seen(cls, username):
+        last_seen = ''
+        with sqla_session() as session:
+            postauth: PostAuth = session.query(PostAuth).filter(PostAuth.username ==
+                                                                username).all()
+            for auth in postauth:
+                last_seen = auth.authdate
+        return last_seen
 
 
 class NasPort(Base):
