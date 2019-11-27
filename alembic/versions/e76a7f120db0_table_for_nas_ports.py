23043121072da5e3c122cd56c74e2c3a7ebb054b
Kristofer Hallin <kristofer@sunet.se>
2019-11-27 10:26:18 +0100

    Let Alembic create autoincrementing index

--- alembic/versions/e76a7f120db0_table_for_nas_ports.py
+++ alembic/versions/e76a7f120db0_table_for_nas_ports.py
@@ -7,6 +7,7 @@ Create Date: 2019-11-21 08:42:10.566995
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.schema import CreateSequence, Sequence
 
 
 # revision identifiers, used by Alembic.
@@ -17,10 +18,22 @@ depends_on = None
 
 
 def upgrade():
+    op.execute(CreateSequence(Sequence('nas_port_id_seq')))
     op.create_table('nas_port',
-                    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
-                    sa.Column('username', sa.Unicode(length=64), nullable=False),
-                    sa.Column('nas_port', sa.Unicode(length=64), nullable=True))
+                    sa.Column('id', sa.Integer(), autoincrement=True,
+                              nullable=False),
+                    sa.Column('username', sa.Unicode(length=64),
+                              nullable=False),
+                    sa.Column('nas_identifier', sa.Unicode(length=64),
+                              nullable=True),
+                    sa.Column('nas_port_id', sa.Unicode(length=64),
+                              nullable=True),
+                    sa.Column('calling_station_id', sa.Unicode(length=64),
+                              nullable=True),
+                    sa.Column('called_station_id', sa.Unicode(length=64),
+                              nullable=True))
+    op.alter_column('nas_port', 'id', nullable=False,
+                    server_default=sa.text("nextval('nas_port_id_seq'::regclass)"))
 
 
 def downgrade():
