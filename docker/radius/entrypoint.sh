a44feae3f42d0b712e690e37a2d1585cf7cc35d1
Kristofer Hallin <kristofer@sunet.se>
2019-12-16 13:54:08 +0100

    Store secrets in Hiera and configuration in the configuration repository.a

--- docker/radius/entrypoint.sh
+++ docker/radius/entrypoint.sh
@@ -1,5 +1,6 @@
-echo ">> Waiting for postgres to start"
+# Make sure we have a connection to Postgres
 WAIT=0
+
 while ! nc -z cnaas_postgres 5432; do
     sleep 1
     WAIT=$(($WAIT + 1))
@@ -9,13 +10,32 @@ while ! nc -z cnaas_postgres 5432; do
       fi
 done
 
+# Create the NAC database
 createdb -h cnaas_postgres -U cnaas nac
 
+# Create tables etc from the stored schema
 psql -h cnaas_postgres -U cnaas nac -f /tmp/schema.sql
+
 if [ $? -ne 0 ]; then
     echo "Error: Failed to create tables"
     exit 1
 fi
 
+# Clone settings from repository
+git clone $GITREPO_ETC /tmp/gitrepo_etc
+cp /tmp/gitrepo_etc/radius/* /etc/freeradius/3.0/
+
+# Replace PSKs when needed
+sed -e "s/EDUROAM_R1_SECRET/$EDUROAM_R1_SECRET/" \
+    -e "s/EDUROAM_R2_SECRET/$EDUROAM_R2_SECRET/" \
+  < /etc/freeradius/3.0/proxy.conf > /tmp/proxy.conf.new \
+  && cat /tmp/proxy.conf.new > /etc/freeradius/3.0/proxy.conf
+sed -e "s/RADIUS_SERVER_SECRET/$RADIUS_SERVER_SECRET/" \
+  < /etc/freeradius/3.0/clients.conf > /tmp/clients.conf.new \
+  && cat /tmp/clients.conf.new > /etc/freeradius/3.0/clients.conf
+
+# Create symlinks
 ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled
+
+# Start freeradius in the foreground with debug enabled
 freeradius -X -f
