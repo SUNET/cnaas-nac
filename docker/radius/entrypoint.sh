287a63dca967da8a5510de5a557511f44cfc47c4
Kristofer Hallin <kristofer@sunet.se>
2020-01-14 12:38:13 +0100

    Simply sleep for 5 seconds before starting RADIUS.

--- docker/radius/entrypoint.sh
+++ docker/radius/entrypoint.sh
@@ -1,21 +1,13 @@
 # Make sure we have a connection to Postgres
 WAIT=0
 
+sleep 5
+
 while ! nc -z cnaas_postgres 5432; do
     sleep 1
     WAIT=$(($WAIT + 1))
       if [ "$WAIT" -gt 15 ]; then
           echo "Error: Timeout wating for Postgres to start"
-          exit 1
-      fi
-done
-
-WAIT=0
-while ! nc -z nac_api 443; do
-    sleep 1
-    WAIT=$(($WAIT + 1))
-      if [ "$WAIT" -gt 15 ]; then
-          echo "Error: Timeout wating for API to start"
           exit 1
       fi
 done
