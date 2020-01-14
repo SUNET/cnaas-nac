b135da130841aba1ea444021a90c30d9f2c517ea
Kristofer Hallin <kristofer@sunet.se>
2020-01-14 09:24:57 +0100

    Remove stuff from __init__

--- src/cnaas_nac/__init__.py
+++ src/cnaas_nac/__init__.py
@@ -1,15 +1,1 @@
-import os
-
-from cnaas_nac.api import app
-
-
 __import__('pkg_resources').declare_namespace(__name__)
-os.environ['PYTHONPATH'] = os.getcwd()
-
-
-def main():
-    app.run(debug=True, host='0.0.0.0')
-
-
-if __name__ == '__main__':
-    main()
