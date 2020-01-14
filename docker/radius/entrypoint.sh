60b1de51202e60412b18baf8f0f9c4c5dc1be141
Kristofer Hallin <kristofer@sunet.se>
2020-01-14 14:09:20 +0100

    Replace more secrets.

--- docker/radius/entrypoint.sh
+++ docker/radius/entrypoint.sh
@@ -42,6 +42,9 @@ sed -e "s/EDUROAM_R1_SECRET/$EDUROAM_R1_SECRET/" \
 sed -e "s/RADIUS_SERVER_SECRET/$RADIUS_SERVER_SECRET/" \
   < /etc/freeradius/3.0/clients.conf > /tmp/clients.conf.new \
   && cat /tmp/clients.conf.new > /etc/freeradius/3.0/clients.conf
+sed -e "s/MDH_ISE_SECRET/$MDH_ISE_SECRET/" \
+  < /etc/freeradius/3.0/clients.conf > /tmp/clients.conf.new \
+  && cat /tmp/clients.conf.new > /etc/freeradius/3.0/clients.conf
 
 # Create symlinks
 ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled
