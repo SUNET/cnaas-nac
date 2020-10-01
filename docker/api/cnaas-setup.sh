ce34d7e1d7583b6c476f4e8b6c2d4bf24f557673
Kristofer Hallin <kristofer@sunet.se>
2020-10-01 14:53:44 +0200

    Checkout this branch when building API.

--- docker/api/cnaas-setup.sh
+++ docker/api/cnaas-setup.sh
@@ -40,7 +40,7 @@ source bin/activate
 # Fetch the code and install dependencies
 git clone https://github.com/SUNET/cnaas-nac.git
 cd cnaas-nac/
-git checkout feature.active_directory
+git checkout feature.coa_port_bounce
 python3 -m pip install -r requirements.txt
 
 # Clean up packages
