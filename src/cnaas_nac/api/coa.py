e4ad6b12cd3562e4d9432bcbe20ae4c5541f5eba
Kristofer Hallin <kristofer@sunet.se>
2020-10-01 14:54:30 +0200

    Embryo of CoA API.

--- src/cnaas_nac/api/coa.py
+++ src/cnaas_nac/api/coa.py
@@ -1,6 +1,27 @@
 from pyrad.client import Client
 from pyrad.dictionary import Dictionary
 
+from flask import request
+from flask_restplus import Resource, Namespace, fields
+
+from cnaas_nac.api.generic import empty_result
+from cnaas_nac.tools.log import get_logger
+
+from cnaas_nac.version import __api_version__
+
+
+logger = get_logger()
+
+
+api = Namespace('coa', description='Port bounce API',
+                prefix='/api/{}'.format(__api_version__))
+
+port_bounce = api.model('bounce', {
+    'portname': fields.String(required=True),
+    'host': fields.String(required=True),
+    'secret': fields.String(required=True)
+})
+
 
 class CoA:
     def __init__(self, host, secret):
@@ -19,6 +40,33 @@ class CoA:
             return 'Failed to send CoA packet: %s (%s)' % (type(e), str(e))
 
         return result
+
+
+class BounceApi(Resource):
+    @api.expect(port_bounce)
+    def post(self):
+        """
+        Send a CoA-Request to the NAS and tell it to flap the selected
+        port.
+
+        The VSA used to flap ports on Airsta switches is
+        Arista-PortFlap and must be present in the dictionary.
+        """
+
+        json_data = request.get_json()
+
+        logger.info(json_data)
+
+        result = {
+            'portname': json_data['portname'],
+            'host': json_data['host'],
+            'secret': '**********'
+        }
+
+        return empty_result(status='success', data=result)
+
+
+api.add_resource(BounceApi)
 
 
 if __name__ == '__main__':
