98c5db4baf8386e3bfb2d5a529740bb4b9279c08
Kristofer Hallin <kristofer@sunet.se>
2020-11-06 10:39:25 +0100

    Shuffle things around even more.

--- src/cnaas_nac/api/internal/app.py
+++ src/cnaas_nac/api/internal/app.py
@@ -2,8 +2,6 @@ import os
 
 from flask import Flask, request, jsonify
 from flask_restplus import Api
-from flask_jwt_extended import JWTManager, decode_token
-from flask_jwt_extended.exceptions import NoAuthorizationError
 
 from cnaas_nac.api.internal.auth import api as auth_api
 from cnaas_nac.version import __api_version__
@@ -15,16 +13,6 @@ from jwt.exceptions import DecodeError, InvalidSignatureError, \
 
 
 logger = get_logger()
-
-
-authorizations = {
-    'apikey': {
-        'type': 'apiKey',
-        'in': 'header',
-        'name': 'Authorization',
-        'description': "Type in 'Bearer: <your JWT token here' to autheticate."
-    }
-}
 
 
 class CnaasApi(Api):
@@ -39,8 +27,6 @@ class CnaasApi(Api):
             # We might catch IndexErrors which are not cuased by JWT,
             # but this is better than nothing.
             data = {'status': 'error', 'data': 'JWT token missing?'}
-        elif isinstance(e, NoAuthorizationError):
-            data = {'status': 'error', 'data': 'JWT token missing?'}
         else:
             return super(CnaasApi, self).handle_error(e)
         return jsonify(data)
@@ -48,27 +34,14 @@ class CnaasApi(Api):
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(128)
-app.config['JWT_PUBLIC_KEY'] = open('certs/public.pem').read()
-app.config['JWT_IDENTITY_CLAIM'] = 'sub'
-app.config['JWT_ALGORITHM'] = 'ES256'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
 
-jwt = JWTManager(app)
-api = CnaasApi(app, prefix='/api/{}'.format(__api_version__),
-               authorizations=authorizations,
-               security='apikey')
-
+api = CnaasApi(app, prefix='/api/{}'.format(__api_version__))
 api.add_namespace(auth_api)
 api.add_namespace(coa_api)
 
 # Log all requests, include username etc
 @app.after_request
 def log_request(response):
-    try:
-        token = request.headers.get('Authorization').split(' ')[-1]
-        user = decode_token(token).get('sub')
-    except Exception:
-        user = 'unknown'
-    logger.info('User: {}, Method: {}, Status: {}, URL: {}, JSON: {}'.format(
-        user, request.method, response.status_code, request.url, request.json))
+    logger.info('[Internal API] Method: {}, Status: {}, URL: {}, JSON: {}'.format(
+        request.method, response.status_code, request.url, request.json))
     return response
--- src/cnaas_nac/api/internal/auth.py
+++ src/cnaas_nac/api/internal/auth.py
@@ -3,7 +3,7 @@ import os
 from flask import request
 from flask_restplus import Resource, Namespace, fields
 
-from cnaas_nac.api.internal.generic import empty_result
+from cnaas_nac.api.generic import empty_result
 from cnaas_nac.tools.log import get_logger
 from cnaas_nac.db.user import User, get_users, UserInfo
 from cnaas_nac.db.oui import DeviceOui
@@ -28,11 +28,6 @@ user_add = api.model('auth', {
     'nas_port_id': fields.String(required=True),
     'calling_station_id': fields.String(required=True),
     'called_station_id': fields.String(required=True)
-})
-
-user_edit = api.model('auth_enable', {
-    'enable': fields.Boolean(required=False),
-    'vlan': fields.String(required=False)
 })
 
 
@@ -269,61 +264,4 @@ class AuthApi(Resource):
         return reject(username, 'User is disabled')
 
 
-class AuthApiByName(Resource):
-    def get(self, username):
-        """
-        Return a JSON blob with all users, VLANs and other information.
-        """
-        return empty_result(status='success', data=get_users(username))
-
-    @api.expect(user_edit)
-    def put(self, username):
-        """
-        Update user parameters such as VLAN, if the user is
-        enabled/disabled and so on.
-        """
-        json_data = request.get_json()
-        result = ''
-
-        if 'enabled' in json_data:
-            if json_data['enabled'] is True:
-                result = User.enable(username)
-            else:
-                result = User.disable(username)
-            UserInfo.add(username, reason='', auth=True)
-        if 'vlan' in json_data:
-            result = Reply.vlan(username, json_data['vlan'])
-        if 'comment' in json_data:
-            result = UserInfo.add(username, comment=json_data['comment'])
-        if result != '':
-            return empty_result(status='error', data=result), 404
-        return empty_result(status='success')
-
-    def delete(self, username):
-        """
-        Remove a user.
-        """
-        errors = []
-        result = User.delete(username)
-        if result != '':
-            errors.append(result)
-
-        result = Reply.delete(username)
-        if result != '':
-            errors.append(result)
-
-        result = NasPort.delete(username)
-        if result != '':
-            errors.append(result)
-
-        result = UserInfo.delete(username)
-        if result != '':
-            errors.append(result)
-
-        if errors != []:
-            return reject(username, errors)
-        return empty_result(status='success')
-
-
 api.add_resource(AuthApi, '')
-api.add_resource(AuthApiByName, '/<string:username>')
