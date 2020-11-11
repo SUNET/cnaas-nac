6f03cb0bfa34efc3bb2302abc7ee77a18b64fcbc
Kristofer Hallin <kristofer@sunet.se>
2020-11-11 08:31:44 +0100

    Check if the table raduserinfo exists before trying to alter it.

--- alembic/versions/425659aef19c_raduserinfo_table.py
+++ alembic/versions/425659aef19c_raduserinfo_table.py
@@ -33,7 +33,7 @@ def upgrade():
                         sa.PrimaryKeyConstraint('id', name='raduserinfo_pkey'))
         op.create_index('raduserinfo_username', 'raduserinfo', ['username'], unique=False)
 
-    if 'authdate' not in inspector.get_columns('raduserinfo'):
+    if 'raduserinfo' in tables and 'authdate' not in inspector.get_columns('raduserinfo'):
         op.add_column('raduserinfo', sa.Column('authdate', postgresql.TIMESTAMP(), server_default=sa.text('CURRENT_TIMESTAMP'), autoincrement=False, nullable=True))
 
     # ### end Alembic commands ###
