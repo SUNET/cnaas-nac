215a1ca264d5a2250df945316f67768644b295c5
Kristofer Hallin <kristofer@sunet.se>
2021-03-26 09:34:05 +0100

    Use certificate and key stored in a Docker volume for Nginx.

--- docker/api/exec-pre-app.sh
+++ docker/api/exec-pre-app.sh
@@ -1,8 +1,14 @@
 cd /opt/cnaas/venv
 . bin/activate
 
+if [ ! -f /opt/cnaas/cert/cert.pem ]; then
+  echo "WARNING: No cert found, using snakeoil (self-signed) certificate and key."
+  cp /opt/cnaas/certs/snakeoil_cert.pem /opt/cnaas/certs/nginx_cert.pem
+  cp /opt/cnaas/certs/snakeoil_key.pem /opt/cnaas/certs/nginx_key.pem
+fi
+
 (cd /opt/cnaas/venv/cnaas-nac/; alembic upgrade head)
 if [ $? -ne 0 ]; then
-    echo "Error: Failed to run Alembic"
+    echo "Error: Failed to run Alembic."
     exit 1
 fi
