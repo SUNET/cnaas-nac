10144adc3b347788cfad4baa2d23d46b7fd80d5c
Kristofer Hallin <kristofer@sunet.se>
2021-12-01 13:03:57 +0100

    Fugly workaround since Cront won't pass any enivironment variables.

--- docker/api/exec-pre-app.sh
+++ docker/api/exec-pre-app.sh
@@ -12,3 +12,19 @@ if [ $? -ne 0 ]; then
     echo "Error: Failed to run Alembic."
     exit 1
 fi
+
+if [ $NAC_REPLICATE_USERNAME ]; then
+    echo "export NAC_REPLICATE_USERNAME=$NAC_REPLICATE_USERNAME" > /root/.replication.sh
+fi
+
+if [ $NAC_REPLICATE_PASSWORD ]; then
+    echo "export NAC_REPLICATE_PASSWORD=$NAC_REPLICATE_PASSWORD" >> /root/.replication.sh
+fi
+
+if [ $NAC_REPLICATE_SOURCE ]; then
+    echo "export NAC_REPLICATE_SOURCE=$NAC_REPLICATE_SOURCE" >> /root/.replication.sh
+fi
+
+if [ $NAC_REPLICATE_TARGET ]; then
+    echo "export NAC_REPLICATE_TARGET=$NAC_REPLICATE_TARGET" >> /root/.replication.sh
+fi
