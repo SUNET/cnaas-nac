3c304f34e2833b6aa6af319897c31b568ceb07e8
Kristofer Hallin <kristofer@sunet.se>
2021-12-07 09:54:15 +0100

    Docker files for development.

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+set -x
+
+export DEBIAN_FRONTEND noninteractive
+
+/bin/sed -i s/deb.debian.org/ftp.se.debian.org/g /etc/apt/sources.list
+
+apt-get update && \
+    apt-get -y dist-upgrade && \
+    apt-get install -y \
+      git \
+      python3-venv \
+      python3-pip \
+      python3-yaml \
+      iputils-ping \
+      procps \
+      bind9-host \
+      netcat-openbsd \
+      net-tools \
+      curl \
+      netcat \
+      nginx \
+      supervisor \
+      libssl-dev \
+      cron \
+      emacs-nox \
+      uwsgi-plugin-python3 \
+    && apt-get clean
+
+pip3 install uwsgi
+
+# Start venv
+python3 -m venv /opt/cnaas/venv
+cd /opt/cnaas/venv/
+source bin/activate
+
+/opt/cnaas/venv/bin/pip3 install -U pip
+
+cd cnaas-nac/
+/opt/cnaas/venv/bin/pip3 install -r /opt/cnaas/requirements.txt
+
+apt-get clean
