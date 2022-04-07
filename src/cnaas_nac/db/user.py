3ac049bf685df7e934793ae73a5ad3d007affa43
Kristofer Hallin <kristofer@sunet.se>
2022-04-07 11:46:00 +0200

    Case insensitive search.

--- src/cnaas_nac/db/user.py
+++ src/cnaas_nac/db/user.py
@@ -8,18 +8,27 @@ from cnaas_nac.db.nas import NasPort
 from cnaas_nac.db.reply import Reply
 from cnaas_nac.db.session import sqla_session
 from cnaas_nac.db.userinfo import UserInfo
-from sqlalchemy import (Boolean, Column, DateTime, Integer, Unicode,
-                        UniqueConstraint, asc, desc, func)
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    Integer,
+    Unicode,
+    UniqueConstraint,
+    asc,
+    desc,
+    func,
+)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'radcheck'
+    __tablename__ = "radcheck"
     __table_args__ = (
         None,
-        UniqueConstraint('id'),
+        UniqueConstraint("id"),
     )
     id = Column(Integer, autoincrement=True, primary_key=True)
     username = Column(Unicode(64), nullable=False)
@@ -44,155 +53,186 @@ class User(Base):
         return d
 
     @classmethod
-    def get(cls, username=''):
+    def get(cls, username=""):
         result = []
         with sqla_session() as session:
             result = []
-            if username == '':
-                query = session.query(User).order_by('username')
+            if username == "":
+                query = session.query(User).order_by("username")
             else:
-                query = session.query(User).filter(User.username ==
-                                                   username).all()
+                query = session.query(User).filter(
+                    User.username == username).all()
             for _ in query:
                 user = _.as_dict()
                 user_dict = dict()
-                user_dict['id'] = user['id']
-                user_dict['username'] = user['username']
-                user_dict['op'] = user['op']
-                user_dict['attribute'] = user['attribute']
+                user_dict["id"] = user["id"]
+                user_dict["username"] = user["username"]
+                user_dict["op"] = user["op"]
+                user_dict["attribute"] = user["attribute"]
                 result.append(user_dict)
         return result
 
     @classmethod
     def add(cls, username, password):
         if cls.get(username) != []:
-            return 'User already exists'
+            return "User already exists"
         with sqla_session() as session:
             new_user = User()
             new_user.username = username
-            new_user.attribute = 'Cleartext-Password'
+            new_user.attribute = "Cleartext-Password"
             new_user.value = password
-            new_user.op = ''
+            new_user.op = ""
             session.add(new_user)
-        return ''
+        return ""
 
     @classmethod
     def enable(cls, username):
         with sqla_session() as session:
-            user: User = session.query(User).filter(User.username ==
-                                                    username).order_by(User.id).one_or_none()
+            user: User = (
+                session.query(User)
+                .filter(User.username == username)
+                .order_by(User.id)
+                .one_or_none()
+            )
             if not user:
-                return 'Username not found'
-            user.attribute = 'Cleartext-Password'
-            user.op = ':='
-        return ''
+                return "Username not found"
+            user.attribute = "Cleartext-Password"
+            user.op = ":="
+        return ""
 
     @classmethod
     def disable(cls, username):
         with sqla_session() as session:
-            user: User = session.query(User).filter(User.username ==
-                                                    username).order_by(User.id).one_or_none()
+            user: User = (
+                session.query(User)
+                .filter(User.username == username)
+                .order_by(User.id)
+                .one_or_none()
+            )
             if not user:
-                return 'Username not found'
-            user.attribute = 'Cleartext-Password'
-            user.op = ''
-        return ''
+                return "Username not found"
+            user.attribute = "Cleartext-Password"
+            user.op = ""
+        return ""
 
     @classmethod
     def is_enabled(cls, username):
         enabled = False
         with sqla_session() as session:
-            user: User = session.query(User).filter(User.username ==
-                                                    username).order_by(User.id).one_or_none()
+            user: User = (
+                session.query(User)
+                .filter(User.username == username)
+                .order_by(User.id)
+                .one_or_none()
+            )
             if not user:
                 return None
-            if user.op == ':=':
+            if user.op == ":=":
                 enabled = True
         return enabled
 
     @classmethod
     def delete(cls, username):
         with sqla_session() as session:
-            instance = session.query(User).filter(User.username ==
-                                                  username).all()
+            instance = session.query(User).filter(
+                User.username == username).all()
             if not instance:
-                return 'Username not found'
+                return "Username not found"
             for _ in instance:
                 session.delete(_)
                 session.commit()
-        return ''
+        return ""
 
     @classmethod
     def password(cls, username, password):
         with sqla_session() as session:
-            user: User = session.query(User).filter(User.username ==
-                                                    username).order_by(User.id).one_or_none()
+            user: User = (
+                session.query(User)
+                .filter(User.username == username)
+                .order_by(User.id)
+                .one_or_none()
+            )
             if not user:
                 return None
             user.value = password
-        return ''
+        return ""
 
 
-def get_users(field=None, condition='', order='', when=None, client_type=None):
+def get_users(field=None, condition="", order="", when=None, client_type=None):
     result = []
 
     db_order = asc(User.username)
     db_field = User.username
-    db_condition = '%{}%'.format(condition)
+    db_condition = "%{}%".format(condition.lower())
     db_when = datetime.now() - timedelta(days=3650)
-    mab_regex = re.compile(r'((?:(\d{1,2}|[a-fA-F]{1,2}){2})(?::|-*)){6}')
+    mab_regex = re.compile(r"((?:(\d{1,2}|[a-fA-F]{1,2}){2})(?::|-*)){6}")
 
     if when is not None:
-        if when == 'hour':
+        if when == "hour":
             db_when = datetime.now() - timedelta(hours=1)
-        elif when == 'day':
+        elif when == "day":
             db_when = datetime.now() - timedelta(days=1)
-        elif when == 'week':
+        elif when == "week":
             db_when = datetime.now() - timedelta(days=7)
-        elif when == 'month':
+        elif when == "month":
             db_when = datetime.now() - timedelta(days=31)
-        elif when == 'year':
+        elif when == "year":
             db_when = datetime.now() - timedelta(days=365)
-        elif when == 'all':
+        elif when == "all":
             db_when = datetime.now() - timedelta(days=3650)
         else:
             db_when = datetime.now() - timedelta(days=3650)
 
     if field is not None:
-        if field == 'username':
-            db_field = User.username
-        elif field == 'vlan':
-            db_field = Reply.value
-        elif field == 'nasip':
-            db_field = NasPort.nas_ip_address
-        elif field == 'nasport':
-            db_field = NasPort.nas_port_id
-        elif field == 'reason':
-            db_field = UserInfo.reason
-        elif field == 'comment':
-            db_field = UserInfo.comment
+        if field == "username":
+            db_field = func.lower(User.username)
+        elif field == "vlan":
+            db_field = func.lower(Reply.value)
+        elif field == "nasip":
+            db_field = func.lower(NasPort.nas_ip_address)
+        elif field == "nasport":
+            db_field = func.lower(NasPort.nas_port_id)
+        elif field == "reason":
+            db_field = func.lower(UserInfo.reason)
+        elif field == "comment":
+            db_field = func.lower(UserInfo.comment)
         else:
             return []
 
-    if order != '':
-        if 'username' in order:
-            db_order = desc(User.username) if order.startswith(
-                '-') else asc(User.username)
-        elif 'vlan' in order:
+    if order != "":
+        if "username" in order:
+            db_order = (
+                desc(User.username) if order.startswith(
+                    "-") else asc(User.username)
+            )
+        elif "vlan" in order:
             db_order = desc(Reply.value) if order.startswith(
-                '-') else asc(Reply.value)
-        elif 'reason' in order:
-            db_order = desc(UserInfo.reason) if order.startswith(
-                '-') else asc(UserInfo.reason)
-        elif 'comment' in order:
-            db_order = desc(UserInfo.authdate) if order.startswith(
-                '-') else asc(UserInfo.authdate)
+                "-") else asc(Reply.value)
+        elif "reason" in order:
+            db_order = (
+                desc(UserInfo.reason) if order.startswith("-") else asc(UserInfo.reason)
+            )
+        elif "comment" in order:
+            db_order = (
+                desc(UserInfo.authdate)
+                if order.startswith("-")
+                else asc(UserInfo.authdate)
+            )
         else:
             db_order = desc(User.username)
 
     with sqla_session() as session:
-        res = session.query(User, Reply, NasPort, UserInfo).filter(User.username == NasPort.username).filter(Reply.username == User.username).filter(
-            Reply.attribute == 'Tunnel-Private-Group-Id').filter(UserInfo.username == User.username).filter(db_field.like(db_condition)).filter(UserInfo.authdate > db_when).order_by(db_order).all()
+        res = (
+            session.query(User, Reply, NasPort, UserInfo)
+            .filter(User.username == NasPort.username)
+            .filter(Reply.username == User.username)
+            .filter(Reply.attribute == "Tunnel-Private-Group-Id")
+            .filter(UserInfo.username == User.username)
+            .filter(db_field.like(db_condition))
+            .filter(UserInfo.authdate > db_when)
+            .order_by(db_order)
+            .all()
+        )
 
         usernames = []
         for user, reply, nas_port, userinfo in res:
@@ -206,34 +246,34 @@ def get_users(field=None, condition='', order='', when=None, client_type=None):
 
             res_dict = dict()
 
-            if client_type == 'mab':
+            if client_type == "mab":
                 if not re.findall(mab_regex, user.username.lower()):
                     continue
-            elif client_type == 'eap':
+            elif client_type == "eap":
                 if re.findall(mab_regex, user.username.lower()):
                     continue
 
-            res_dict['username'] = user.username
+            res_dict["username"] = user.username
 
-            if user.op == ':=':
-                res_dict['active'] = True
+            if user.op == ":=":
+                res_dict["active"] = True
             else:
-                res_dict['active'] = False
-            res_dict['vlan'] = reply.value
-            res_dict['nas_identifier'] = nas_port.nas_identifier
-            res_dict['nas_port_id'] = nas_port.nas_port_id
-            res_dict['nas_ip_address'] = nas_port.nas_ip_address
-            res_dict['calling_station_id'] = nas_port.calling_station_id
-            res_dict['called_station_id'] = nas_port.called_station_id
-            res_dict['comment'] = userinfos[user.username]['comment']
-            res_dict['reason'] = userinfos[user.username]['reason']
+                res_dict["active"] = False
+            res_dict["vlan"] = reply.value
+            res_dict["nas_identifier"] = nas_port.nas_identifier
+            res_dict["nas_port_id"] = nas_port.nas_port_id
+            res_dict["nas_ip_address"] = nas_port.nas_ip_address
+            res_dict["calling_station_id"] = nas_port.calling_station_id
+            res_dict["called_station_id"] = nas_port.called_station_id
+            res_dict["comment"] = userinfos[user.username]["comment"]
+            res_dict["reason"] = userinfos[user.username]["reason"]
 
-            if 'authdate' not in userinfos[user.username]:
-                authdate = ''
+            if "authdate" not in userinfos[user.username]:
+                authdate = ""
             else:
-                authdate = str(userinfos[user.username]['authdate'])
+                authdate = str(userinfos[user.username]["authdate"])
 
-            res_dict['authdate'] = authdate
+            res_dict["authdate"] = authdate
             result.append(res_dict)
 
     return result
