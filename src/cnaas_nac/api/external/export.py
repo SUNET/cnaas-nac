7c1a71e58bdb14b9867d790a310a6540097fac7f
Kristofer Hallin <kristofer@sunet.se>
2022-09-27 14:25:46 +0200

    API endpoint for exporting data.

---
+++ src/cnaas_nac/api/external/export.py
@@ -0,0 +1,41 @@
+from cnaas_nac.api.generic import csv_export, empty_result
+from cnaas_nac.db.user import get_users
+from cnaas_nac.tools.log import get_logger
+from cnaas_nac.version import __api_version__
+from flask import jsonify, make_response
+from flask_jwt_extended import decode_token
+from flask_restx import Namespace, Resource
+
+logger = get_logger()
+
+
+api = Namespace(
+    "export",
+    description="Export API",
+    prefix="/api/{}".format(__api_version__),
+)
+
+
+class ExportApi(Resource):
+    def get(self, token):
+        try:
+            decode_token(token)
+        except Exception as e:
+            return make_response(
+                jsonify(empty_result(status="error", data=str(e))), 400
+            )
+
+        users = get_users()
+        user_count = len(users)
+
+        response = make_response(csv_export(users))
+        response.headers["Content-type"] = "application/csv"
+        response.headers[
+            "Content-Disposition"
+        ] = "attachment; filename=export.csv"
+        response.headers["X-Total-Count"] = user_count
+
+        return response
+
+
+api.add_resource(ExportApi, "/<string:token>")
