10c7cc6fb7aef2cad34ccbae16bed0af44c9f7e7
Kristofer Hallin <kristofer@sunet.se>
2022-11-26 15:54:29 +0100

    Use flask_apscheduler.

--- src/cnaas_nac/api/internal/app.py
+++ src/cnaas_nac/api/internal/app.py
@@ -1,9 +1,11 @@
 import os
 
 from cnaas_nac.api.internal.auth import api as auth_api
+from cnaas_nac.tools.cleanup import accounting_cleanup, users_cleanup
 from cnaas_nac.tools.log import get_logger
 from cnaas_nac.version import __api_version__
 from flask import Flask, jsonify, request
+from flask_apscheduler import APScheduler
 from flask_restx import Api
 from jwt.exceptions import (DecodeError, InvalidSignatureError,
                             InvalidTokenError)
@@ -34,7 +36,10 @@ app.config['SECRET_KEY'] = os.urandom(128)
 api = CnaasApi(app, prefix='/api/{}'.format(__api_version__))
 api.add_namespace(auth_api)
 
-# Log all requests, include username etc
+
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
 
 
 @app.after_request
@@ -42,3 +47,13 @@ def log_request(response):
     logger.info('[Internal API] Method: {}, Status: {}, URL: {}'.format(
         request.method, response.status_code, request.url))
     return response
+
+
+@scheduler.task('interval', id='cleanup_users', seconds=900, misfire_grace_time=900)
+def users():
+    users_cleanup()
+
+
+@scheduler.task('interval', id='cleanup_accounting', seconds=3600, misfire_grace_time=900)
+def accounting():
+    accounting_cleanup()
--- src/cnaas_nac/run_internal.py
+++ src/cnaas_nac/run_internal.py
@@ -1,19 +1,8 @@
 import os
 
 from cnaas_nac.api.internal import app
-from cnaas_nac.tools.cleanup import accounting_cleanup, users_cleanup
-from cnaas_nac.tools.scheduler import Scheduler
 
 os.environ['PYTHONPATH'] = os.getcwd()
-
-
-def run_scheduler():
-    scheduler = Scheduler()
-    scheduler.add(users_cleanup, job_id="user cleanup",
-                  interval=900, maxruns=0)
-    scheduler.add(accounting_cleanup, job_id="accounting cleanup",
-                  interval=3600, maxruns=0)
-    scheduler.start()
 
 
 def run_app():
@@ -21,8 +10,7 @@ def run_app():
 
 
 if __name__ == '__main__':
-    run_scheduler()
     run_app().run(debug=True, host='0.0.0.0', port=5002)
+
 else:
-    run_scheduler()
     cnaas_app = run_app()
