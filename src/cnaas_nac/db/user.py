af412de3693c402c1af7975d42d454c0c9938270
Kristofer Hallin <kristofer@sunet.se>
2023-09-05 11:10:24 +0200

    Re-used DB sessions in order to prevent too many connections.

--- src/cnaas_nac/db/user.py
+++ src/cnaas_nac/db/user.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 from cnaas_nac.db.groups import Group
 from cnaas_nac.db.nas import NasPort
 from cnaas_nac.db.reply import Reply
-from cnaas_nac.db.session import sqla_session
+from cnaas_nac.db.session import get_session
 from cnaas_nac.db.userinfo import UserInfo
 from cnaas_nac.tools.ouis import ouis
 from sqlalchemy import (Column, Integer, Unicode, UniqueConstraint, asc, desc,
@@ -13,6 +13,7 @@ from sqlalchemy import (Column, Integer, Unicode, UniqueConstraint, asc, desc,
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+session = get_session()
 
 
 class User(Base):
@@ -47,107 +48,100 @@ class User(Base):
     @classmethod
     def get(cls, username=""):
         result = []
-        with sqla_session() as session:
-            result = []
-            if username == "":
-                query = session.query(User).order_by("username")
-            else:
-                query = session.query(User).filter(
-                    User.username == username).all()
-            for _ in query:
-                user = _.as_dict()
-                user_dict = dict()
-                user_dict["id"] = user["id"]
-                user_dict["username"] = user["username"]
-                user_dict["password"] = ""
-                user_dict["op"] = user["op"]
-                user_dict["attribute"] = user["attribute"]
-                result.append(user_dict)
+        result = []
+        if username == "":
+            query = session.query(User).order_by("username")
+        else:
+            query = session.query(User).filter(
+                User.username == username).all()
+        for _ in query:
+            user = _.as_dict()
+            user_dict = dict()
+            user_dict["id"] = user["id"]
+            user_dict["username"] = user["username"]
+            user_dict["password"] = ""
+            user_dict["op"] = user["op"]
+            user_dict["attribute"] = user["attribute"]
+            result.append(user_dict)
         return result
 
     @classmethod
     def add(cls, username, password):
         if cls.get(username) != []:
             return f"User {username} already exists"
-        with sqla_session() as session:
-            new_user = User()
-            new_user.username = username
-            new_user.attribute = "Cleartext-Password"
-            new_user.value = password
-            new_user.op = ""
-            session.add(new_user)
+        new_user = User()
+        new_user.username = username
+        new_user.attribute = "Cleartext-Password"
+        new_user.value = password
+        new_user.op = ""
+        session.add(new_user)
         return ""
 
     @classmethod
     def enable(cls, username):
-        with sqla_session() as session:
-            user: User = (
-                session.query(User)
-                .filter(User.username == username)
-                .order_by(User.id)
-                .one_or_none()
-            )
-            if not user:
-                return f"Username {username} not found"
-            user.attribute = "Cleartext-Password"
-            user.op = ":="
+        user: User = (
+            session.query(User)
+            .filter(User.username == username)
+            .order_by(User.id)
+            .one_or_none()
+        )
+        if not user:
+            return f"Username {username} not found"
+        user.attribute = "Cleartext-Password"
+        user.op = ":="
         return ""
 
     @classmethod
     def disable(cls, username):
-        with sqla_session() as session:
-            user: User = (
-                session.query(User)
-                .filter(User.username == username)
-                .order_by(User.id)
-                .one_or_none()
-            )
-            if not user:
-                return f"Username {username} not found"
-            user.attribute = "Cleartext-Password"
-            user.op = ""
+        user: User = (
+            session.query(User)
+            .filter(User.username == username)
+            .order_by(User.id)
+            .one_or_none()
+        )
+        if not user:
+            return f"Username {username} not found"
+        user.attribute = "Cleartext-Password"
+        user.op = ""
         return ""
 
     @classmethod
     def is_enabled(cls, username):
         enabled = False
-        with sqla_session() as session:
-            user: User = (
-                session.query(User)
-                .filter(User.username == username)
-                .order_by(User.id)
-                .one_or_none()
-            )
-            if not user:
-                return None
-            if user.op == ":=":
-                enabled = True
+        user: User = (
+            session.query(User)
+            .filter(User.username == username)
+            .order_by(User.id)
+            .one_or_none()
+        )
+        if not user:
+            return None
+        if user.op == ":=":
+            enabled = True
         return enabled
 
     @classmethod
     def delete(cls, username):
-        with sqla_session() as session:
-            instance = session.query(User).filter(
-                User.username == username).all()
-            if not instance:
-                return f"Username {username} not found"
-            for _ in instance:
-                session.delete(_)
-                session.commit()
+        instance = session.query(User).filter(
+            User.username == username).all()
+        if not instance:
+            return f"Username {username} not found"
+        for _ in instance:
+            session.delete(_)
+            session.commit()
         return ""
 
     @classmethod
     def password(cls, username, password):
-        with sqla_session() as session:
-            user: User = (
-                session.query(User)
-                .filter(User.username == username)
-                .order_by(User.id)
-                .one_or_none()
-            )
-            if not user:
-                return None
-            user.value = password
+        user: User = (
+            session.query(User)
+            .filter(User.username == username)
+            .order_by(User.id)
+            .one_or_none()
+        )
+        if not user:
+            return None
+        user.value = password
         return ""
 
 
@@ -218,59 +212,58 @@ def get_users(field=None, condition="", order="", when=None, client_type=None,
 
     userinfos = UserInfo.get(usernames=usernames_list)
 
-    with sqla_session() as session:
-        res = (
-            session.query(User, Reply, NasPort, UserInfo)
-            .filter(User.username == NasPort.username)
-            .filter(Reply.username == User.username)
-            .filter(Reply.attribute == "Tunnel-Private-Group-Id")
-            .filter(UserInfo.username == User.username)
-            .filter(db_field.like(db_condition))
-            .filter(UserInfo.authdate > db_when)
-            .order_by(db_order)
-            .all()
-        )
+    res = (
+        session.query(User, Reply, NasPort, UserInfo)
+        .filter(User.username == NasPort.username)
+        .filter(Reply.username == User.username)
+        .filter(Reply.attribute == "Tunnel-Private-Group-Id")
+        .filter(UserInfo.username == User.username)
+        .filter(db_field.like(db_condition))
+        .filter(UserInfo.authdate > db_when)
+        .order_by(db_order)
+        .all()
+    )
 
-        for user, reply, nas_port, userinfo in res:
-            if usernames_list != []:
-                if user.username not in usernames_list:
-                    continue
+    for user, reply, nas_port, userinfo in res:
+        if usernames_list != []:
+            if user.username not in usernames_list:
+                continue
 
-            res_dict = dict()
-            res_dict["username"] = user.username
-            res_dict["password"] = ""
+        res_dict = dict()
+        res_dict["username"] = user.username
+        res_dict["password"] = ""
 
-            if user.op == ":=":
-                res_dict["active"] = True
-            else:
-                res_dict["active"] = False
-            res_dict["vlan"] = reply.value
-            res_dict["nas_identifier"] = nas_port.nas_identifier
-            res_dict["nas_port_id"] = nas_port.nas_port_id
-            res_dict["nas_ip_address"] = nas_port.nas_ip_address
-            res_dict["calling_station_id"] = nas_port.calling_station_id
-            res_dict["called_station_id"] = nas_port.called_station_id
-            res_dict["comment"] = userinfos[user.username]["comment"]
-            res_dict["reason"] = userinfos[user.username]["reason"]
-            res_dict["access_start"] = userinfos[user.username]["access_start"]
-            res_dict["access_stop"] = userinfos[user.username]["access_stop"]
-            res_dict["access_restricted"] = userinfos[user.username]["access_restricted"]
-            res_dict["accepts"] = userinfos[user.username]["accepts"]
-            res_dict["rejects"] = userinfos[user.username]["rejects"]
+        if user.op == ":=":
+            res_dict["active"] = True
+        else:
+            res_dict["active"] = False
+        res_dict["vlan"] = reply.value
+        res_dict["nas_identifier"] = nas_port.nas_identifier
+        res_dict["nas_port_id"] = nas_port.nas_port_id
+        res_dict["nas_ip_address"] = nas_port.nas_ip_address
+        res_dict["calling_station_id"] = nas_port.calling_station_id
+        res_dict["called_station_id"] = nas_port.called_station_id
+        res_dict["comment"] = userinfos[user.username]["comment"]
+        res_dict["reason"] = userinfos[user.username]["reason"]
+        res_dict["access_start"] = userinfos[user.username]["access_start"]
+        res_dict["access_stop"] = userinfos[user.username]["access_stop"]
+        res_dict["access_restricted"] = userinfos[user.username]["access_restricted"]
+        res_dict["accepts"] = userinfos[user.username]["accepts"]
+        res_dict["rejects"] = userinfos[user.username]["rejects"]
 
-            user_oui = user.username[:8]
+        user_oui = user.username[:8]
 
-            try:
-                res_dict["vendor"] = ouis[user_oui]
-            except KeyError:
-                res_dict["vendor"] = ""
+        try:
+            res_dict["vendor"] = ouis[user_oui]
+        except KeyError:
+            res_dict["vendor"] = ""
 
-            if "authdate" not in userinfos[user.username]:
-                authdate = ""
-            else:
-                authdate = str(userinfos[user.username]["authdate"])
+        if "authdate" not in userinfos[user.username]:
+            authdate = ""
+        else:
+            authdate = str(userinfos[user.username]["authdate"])
 
-            res_dict["authdate"] = authdate
-            result.append(res_dict)
+        res_dict["authdate"] = authdate
+        result.append(res_dict)
 
     return result
